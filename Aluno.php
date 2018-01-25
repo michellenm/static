797186bb76a5d1c2fb6f34a18acd524e0de777d2
php 24 <php.24@daphne.cec.ime.usp.br>
2018-01-24 22:38:01 -0200

    metodos da classe by Prof

---
+++ Aluno.php
@@ -0,0 +1,19 @@
+<?php
+use Carbon\Carbon;
+class Aluno
+{
+  public $nome; // atributo de objeto
+  public $dataDeNascimento; // atributo de objeto
+  public $rg;
+  public $turma;
+  //atributo de classe
+  public static $contador = 0;
+  // construtor
+  function __construct(string $nome, Carbon $dataDeNascimento)
+  {
+    $this->nome = $nome;
+    $this->dataDeNascimento = $dataDeNascimento;
+    self::$contador++;
+    echo "Contador: " . Aluno::$contador . PHP_EOL;
+  }
+}
---
+++ TestaRelacaoAlunoTurma.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+require 'lib/Carbon.php';
+use Carbon\Carbon;
+require 'Turma.php';
+require 'Aluno.php';
+$turma = new Turma();
+$turma->periodo = "Manhã";
+$turma->serie = 5;
+$turma->sigla = "B";
+$turma->tipoDeEnsino = "Fundamental";
+$aluno = new Aluno("Rafael", Carbon::createFromDate(1984, 10, 30));
+$aluno = new Aluno("Jonas", Carbon::createFromDate(1984, 10, 30));
+$aluno = new Aluno("Gustavo", Carbon::createFromDate(1984, 10, 30));
+// $aluno->turma = $turma;
+//
+// echo "Aluno: $aluno->nome" . PHP_EOL;
+// echo "Mês de nascimento:" .
+//     $aluno->dataDeNascimento->month
+//      . PHP_EOL;
+// echo "Periodo: {$aluno->turma->periodo}" . PHP_EOL;
